a6288d084b514d9c2cc49abd34caa91c9ecf5c9a
unknown <sushant_bhasin@CHDSEZ301824D.ad.infosys.com>
2014-12-03 16:48:31 +0530

    Test Commit

---
+++ src/com/example/robotium_ex/FullscreenActivity.java
@@ -0,0 +1,68 @@
+package com.example.robotium_ex;
+
+import com.example.robotium_ex.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * 
+ * @see SystemUiHider
+ */
+public class FullscreenActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_fullscreen);
+		final Button toast=(Button) findViewById(R.id.button1);
+		final Button nAct=(Button) findViewById(R.id.button2);
+		
+
+		// Set up an instance of SystemUiHider to control the system UI for
+		// this activity.
+			toast.setOnClickListener(new Button.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast t=Toast.makeText(getApplicationContext(), "Jenkins Robotium test",Toast.LENGTH_LONG);
+				t.show();
+			}
+		});
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+	}
+
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	
+}
